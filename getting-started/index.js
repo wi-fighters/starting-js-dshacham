2ef72a5cc58797c5701da4cd0de70cdd9fad04e4
dshacham <dshacham22@gmail.com>
2019-08-13 10:31:36 +0200

    add num 6

--- getting-started/index.js
+++ getting-started/index.js
@@ -1,35 +1,40 @@
-
+//1
 var firstName = 'Dana';
-
 var lastName = 'Shacham';
-
 console.log(firstName,lastName);
 
+//2
 var fullName = 'Dana Shacham';
-
 var myAge = 32;
-
 var myJob = 'student';
-
 var myCity = 'Berlin';
-
 var favAnimal = 'cats';
 
+//3
 console.log(fullName + ' is a ' + myAge + ' year old ' + myJob + ' who lives in ' + myCity + ' and loves ' + favAnimal);
 
+//4
 let Name = 'Dana Shacham'; //(string)
 var myAge = 32; //(number)
 var myJob = 'student'; //(string)
 var myCity = 'Berlin'; //(string)
 var favAnimal = 'cats'; //(string)
-
 console.log(typeof Name);
 console.log(typeof myAge);
 console.log(typeof myJob);
 console.log(typeof myCity);
 console.log(typeof favAnimal);
 
+//5
 let ageMark = '50';
 let ageJohn = 30;
+console.log(typeof ageMark);
+console.log(typeof ageJohn);
 
-console.log(ageMark, ageJohn);
+ageMark = 50;
+
+//6
+let whoIsOlder = ageMark > ageJohn;
+
+console.log('Mark is older than John:', whoIsOlder);
+
