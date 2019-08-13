cb54cfcb93feecee03f61d82e11f9d15f512f179
dshacham <dshacham22@gmail.com>
2019-08-13 15:31:32 +0200

    finished exercise

--- arithmetic/index.js
+++ arithmetic/index.js
@@ -1,1 +1,40 @@
-//Title
+//1
+let result = (17 / 100) * 30;
+console.log(result);
+
+//2
+let elmt = [8+6+5+9+8+2+1+8.5+7+4]
+console.log(elmt / 10);
+
+//3
+let result2 = 6*85-75-70-85-90-100;
+console.log(result2);
+
+//4
+let first = 8*78;
+let second = 9*80;
+let result3 = second - first;
+console.log(result3);
+
+//5
+console.log(22.35/100*10 + 22.35);
+console.log(26.67/100*15 + 26.67);
+console.log(35.92/100*20 + 35.92);
+
+//6
+let result4 = 4.75*4;
+console.log(result4+'cm');
+
+let result5 = 5+6+7;
+console.log(result5+'cm');
+
+let result6 = 5*5;
+console.log(result6+'cm');
+
+let result7 = 9*3;
+console.log(result7 + 'cm');
+
+//7
+console.log(6450/15);
+
+console.log(500*52);
