d031bb9076f7c187ea3a52700fdb2a568855b7df
dshacham <dshacham22@gmail.com>
2019-08-13 10:10:58 +0200

    fixing the missing file problem

--- getting-started/index.js
+++ getting-started/index.js
@@ -1,1 +1,35 @@
 
+var firstName = 'Dana';
+
+var lastName = 'Shacham';
+
+console.log(firstName,lastName);
+
+var fullName = 'Dana Shacham';
+
+var myAge = 32;
+
+var myJob = 'student';
+
+var myCity = 'Berlin';
+
+var favAnimal = 'cats';
+
+console.log(fullName + ' is a ' + myAge + ' year old ' + myJob + ' who lives in ' + myCity + ' and loves ' + favAnimal);
+
+let Name = 'Dana Shacham'; //(string)
+var myAge = 32; //(number)
+var myJob = 'student'; //(string)
+var myCity = 'Berlin'; //(string)
+var favAnimal = 'cats'; //(string)
+
+console.log(typeof Name);
+console.log(typeof myAge);
+console.log(typeof myJob);
+console.log(typeof myCity);
+console.log(typeof favAnimal);
+
+let ageMark = '50';
+let ageJohn = 30;
+
+console.log(ageMark, ageJohn);
