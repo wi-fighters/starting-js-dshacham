349810e8ed0247d317de58aa86d5fea59f611d81
dshacham <dshacham22@gmail.com>
2019-08-14 14:59:17 +0200

    first commit

--- string-methods/index.js
+++ string-methods/index.js
@@ -1,1 +1,15 @@
+// 1
+const stringOne = 'this sucks';
+console.log(stringOne.length); // 10
+console.log(stringOne[1]);
+console.log(stringOne[5]);
 
+// 2
+const ex2 = 'JavaScript';
+console.log(ex2.length); // 10
+console.log(ex2[0]);
+console.log(ex2[4]);
+
+// 3
+const ex3 = 'I can walk in the park all day!';
+console.log(ex3.substring(18, 22));
