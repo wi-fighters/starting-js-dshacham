d8444d1d781da694c2162e7d047227effc7b4996
Dana <53760794+dshacham@users.noreply.github.com>
2019-08-15 09:54:30 +0200

    first commit

---
+++ name-analyzer/index.js
@@ -0,0 +1,20 @@
+// 1
+const userName = 'dshacham';
+console.log('Hello ' + userName + '.');
+console.log('Your user name is ' + userName.length + ' letters long.');
+console.log('The first letter of your user name is ' + userName[0] + ', the last is ' + userName[userName.length - 1] + '.');
+console.log('If I wanted to be funny, I would call you ' + userName.substring(0, 4) + 'moo.');
+
+// 2
+const userName = 'dshacham superstar'; // not working
+console.log('Hello ' + userName + '.');
+console.log('Your user name is ' + userName.length + ' letters long.');
+console.log('The first letter of your user name is ' + userName[0] + ', the last is ' + userName[userName.length - 1] + '.');
+console.log('If I wanted to be funny, I would call you ' + userName.substring(0, 4) + 'moo.');
+
+// 3
+const userName = 'dshacham';
+console.log('Hello ' + userName + '.');
+console.log('Your user name is ' + userName.length + ' letters long.');
+console.log('The first letter of your user name is ' + userName[0] + ', the last is ' + userName[userName.length - 1] + '.');
+console.log('If I wanted to be funny, I would call you ' + userName.substring(0, 4) + 'moo.');
