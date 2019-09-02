e8db04363aa6c5c5ce8ca4c0034df74b20abe8af
dshacham <dshacham22@gmail.com>
2019-09-02 15:25:09 +0200

    push to save work

--- band-name/band-name-ex.js
+++ band-name/band-name-ex.js
@@ -1,13 +1,57 @@
-function generateBandName(clothingColor, lastFoodEaten) {
-    const colorCapital = clothingColor[0].toUpperCase();
-    const colorRest = clothingColor.slice(1).toLowerCase();
-    const color = colorCapital.concat(colorRest);
+// function generateBandName(clothingColor, lastFoodEaten) {
+//     const colorCapital = clothingColor[0].toUpperCase();
+//     const colorRest = clothingColor.slice(1).toLowerCase();
+//     const color = colorCapital.concat(colorRest);
 
-    const lfeCapital = lastFoodEaten[0].toUpperCase();
-    const lfeRest = lastFoodEaten.slice(1).toLowerCase();
-    const food = lfeCapital.concat(lfeRest);
+//     const lfeCapital = lastFoodEaten[0].toUpperCase();
+//     const lfeRest = lastFoodEaten.slice(1).toLowerCase();
+//     const food = lfeCapital.concat(lfeRest);
 
-    return `The ${color} ${food}`;
+//     return `The ${color} ${food}`;
+// }
+// console.log(generateBandName('purple', 'pie'));
+
+// function bandName(clothingColor, lastFoodEaten) {
+//     return `The ${clothingColor[0].toUpperCase() + clothingColor.slice(1).toLowerCase()} ${lastFoodEaten[0].toUpperCase() + lastFoodEaten.slice(1).toLowerCase()}`
+// }
+
+// console.log(bandName('yeLLOW', 'BROKOLI'));
+
+// function generateBandName(clothingColor, lastFoodEaten) {
+//     const bandNames = [];
+//     bandNames.push(`The ${clothingColor[0].toUpperCase() + clothingColor.slice(1).toLowerCase()} ${lastFoodEaten[0].toUpperCase() + lastFoodEaten.slice(1).toLowerCase()}`);
+//     return bandNames;
+// }
+
+// console.log(generateBandName('greEn', 'CHOCOLATE'));
+// console.log(generateBandName('BLACK', 'pasta'));
+
+// function generateBandName(clothingColor, lastFoodEaten) {
+//     bandNames.push(`The ${clothingColor[0].toUpperCase() + clothingColor.slice(1).toLowerCase()} ${lastFoodEaten[0].toUpperCase() + lastFoodEaten.slice(1).toLowerCase()}`);
+//     return bandNames;
+// }
+// const bandNames = []
+
+// generateBandName('greEn', 'CHOCOLATE');
+// generateBandName('BLACK', 'pasta');
+// console.log(bandNames);
+
+// function generateBandName(clothingColor, lastFoodEaten) {
+//     const bandName = '';
+//     return `The ${clothingColor[0].toUpperCase() + clothingColor.slice(1).toLowerCase()} ${lastFoodEaten[0].toUpperCase() + lastFoodEaten.slice(1).toLowerCase()}`;
+// }
+
+// console.log(generateBandName('BLACK', 'pasta'));
+
+function capitalizeColor(clothingColor) {
+    const capiCol = '';
+    return `The ${clothingColor[0].toUpperCase() + lastFoodEaten.slice(1).toLowerCase}`;
 }
 
-console.log(generateBandName('purple', 'pie'));
+function capitalizeFood(lastFoodEaten) {
+    const capiFood = '';
+    return `${lastFoodEaten[0].toUpperCase() + lastFoodEaten.slice(1).toLowerCase}`;
+}
+
+console.log(capitalizeColor('rED'));
+console.log(capitalizeFood('stEw'));
