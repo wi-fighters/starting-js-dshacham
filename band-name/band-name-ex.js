8d53ce565605ecd2bb66521ed277d3fdfa72c15b
dshacham <dshacham22@gmail.com>
2019-09-02 13:45:13 +0200

    first part of exercise

--- band-name/band-name-ex.js
+++ band-name/band-name-ex.js
@@ -1,1 +1,13 @@
-start here
+function generateBandName(clothingColor, lastFoodEaten) {
+    const colorCapital = clothingColor[0].toUpperCase();
+    const colorRest = clothingColor.slice(1).toLowerCase();
+    const color = colorCapital.concat(colorRest);
+
+    const lfeCapital = lastFoodEaten[0].toUpperCase();
+    const lfeRest = lastFoodEaten.slice(1).toLowerCase();
+    const food = lfeCapital.concat(lfeRest);
+
+    return `The ${color} ${food}`;
+}
+
+console.log(generateBandName('purple', 'pie'));
