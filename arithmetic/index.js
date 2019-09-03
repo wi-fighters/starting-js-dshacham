ef4e460c4cc32853b67f2076fd11a14f35f37e07
dshacham <dshacham22@gmail.com>
2019-09-03 13:31:39 +0200

    final result

--- arithmetic/index.js
+++ arithmetic/index.js
@@ -1,59 +1,62 @@
 // 1
-const stringOne = 'this sucks';
+const stringOne = "this sucks";
 console.log(stringOne.length); // 10
 console.log(stringOne[1]);
 console.log(stringOne[5]);
 
 // 2
-const ex2 = 'JavaScript';
+const ex2 = "JavaScript";
 console.log(ex2.length); // 10
 console.log(ex2[0]);
 console.log(ex2[4]);
 
 // 3
-const ex3 = 'I can walk in the park all day!';
+const ex3 = "I can walk in the park all day!";
 console.log(ex3.substring(18, 22));
 
 // 4
-const ex4 = 'JavaScript';
+const ex4 = "JavaScript";
 console.log(ex4.substring(3, 6));
 
 // 5
-const ex5 = 'Hello World';
+const ex5 = "Hello World";
 console.log(ex5.toUpperCase());
 
 // 6
-const ex6 = 'Hello Earthling';
+const ex6 = "Hello Earthling";
 console.log(ex6.toLowerCase());
 
 // 7
-const ex7 = 'nice shoes';
+const ex7 = "nice shoes";
 console.log(ex7.includes("l"));
 console.log(ex7.includes("n"));
 
 //8
-const ex8 = 'JavaScript';
+const ex8 = "JavaScript";
 console.log(`${ex8[0]}${ex8}${ex8[0]}`);
 
 //9
-const ex9 = 'JavaScript';
+const ex9 = "JavaScript";
 console.log(`${ex9.substring(7)}${ex8}${ex9.substring(7)}`);
 
 //10
-const ex10 = 'JavaScript';
-console.log(ex10.toUpperCase()); 
-console.log(ex10.includes('Java'));
+const ex10 = "JavaScript";
+console.log(ex10.toUpperCase());
+console.log(ex10.includes("Java"));
 
 //11
-const ex11 = 'JavaScript';
-console.log(`${ex11[9]}${ex11.substring(1,9)}${ex11[0]}`);
+const ex11 = "JavaScript";
+console.log(`${ex11[9]}${ex11.substring(1, 9)}${ex11[0]}`);
 
 //12
-const firstName = 'Dana';
-const myCity = 'Berlin';
-const myHobby = 'listening to music';
-console.log(`My name is ${firstName}, I live in ${myCity} and my hobby is ${myHobby}.`);
+const firstName = "Dana";
+const myCity = "Berlin";
+
+const myHobby = "listening to music";
+console.log(
+  `My name is ${firstName}, I live in ${myCity} and my hobby is ${myHobby}.`
+);
 
 //13
-const ex13 = 'the quick brown fox';
+const ex13 = "the quick brown fox";
 console.log(ex13.charAt(0).toUpperCase() + ex13.slice(1));
--- band-name/band-name-ex.js
+++ band-name/band-name-ex.js
@@ -1,3 +1,4 @@
+// 1
 // function generateBandName(clothingColor, lastFoodEaten) {
 //     const colorCapital = clothingColor[0].toUpperCase();
 //     const colorRest = clothingColor.slice(1).toLowerCase();
@@ -11,21 +12,14 @@
 // }
 // console.log(generateBandName('purple', 'pie'));
 
+// 1.0
 // function bandName(clothingColor, lastFoodEaten) {
 //     return `The ${clothingColor[0].toUpperCase() + clothingColor.slice(1).toLowerCase()} ${lastFoodEaten[0].toUpperCase() + lastFoodEaten.slice(1).toLowerCase()}`
 // }
 
 // console.log(bandName('yeLLOW', 'BROKOLI'));
 
-// function generateBandName(clothingColor, lastFoodEaten) {
-//     const bandNames = [];
-//     bandNames.push(`The ${clothingColor[0].toUpperCase() + clothingColor.slice(1).toLowerCase()} ${lastFoodEaten[0].toUpperCase() + lastFoodEaten.slice(1).toLowerCase()}`);
-//     return bandNames;
-// }
-
-// console.log(generateBandName('greEn', 'CHOCOLATE'));
-// console.log(generateBandName('BLACK', 'pasta'));
-
+// 1.1
 // function generateBandName(clothingColor, lastFoodEaten) {
 //     bandNames.push(`The ${clothingColor[0].toUpperCase() + clothingColor.slice(1).toLowerCase()} ${lastFoodEaten[0].toUpperCase() + lastFoodEaten.slice(1).toLowerCase()}`);
 //     return bandNames;
@@ -43,37 +37,84 @@
 
 // console.log(generateBandName('BLACK', 'pasta'));
 
+// 2
 // function generateBandName(clothingColor, lastFoodEaten) {
 //     let bandName = '';
 
-//     let color = capitalizeColor(clothingColor);
-//     let food = capitalizeFood(lastFoodEaten);
+//     const color = function () {
+//         return clothingColor[0].toUpperCase() + clothingColor.slice(1).toLowerCase();
+//     };
+//     const food = function () {
+//         return lastFoodEaten[0].toUpperCase() + lastFoodEaten.slice(1).toLowerCase();
+//     };
 
-//     bandName = `The ${color} ${food}!`;
+//     bandName = `The ${color()} ${food()}!`;
 
 //     return bandName;
 // }
+// console.log(generateBandName('gRay', 'APRICOT'));
+// function generateBandName(clothingColor, lastFoodEaten) {
+//     let bandName = '';
 
+//     const color = capitalizeColor(clothingColor);
+//     const food = capitalizeFood(lastFoodEaten);
+
+//     bandName = `The ${color()} ${food()}!`;
+
+//     return bandName;
+// }
 // function capitalizeColor(clothingColor) {
-//     return clothingColor[0].toUpperCase() + clothingColor.substr(1).toLowerCase();
+//     return clothingColor[0].toUpperCase() + clothingColor.slice(1).toLowerCase()
 // }
 
-// function capitalizeFood(lastFoodEaten) {
-//     return lastFoodEaten[0].toUpperCase() + lastFoodEaten.substr(1).toLowerCase();
+// function capitalizeFood(clothingColor) {
+//     return lastFoodEaten[0].toUpperCase() + lastFoodEaten.slice(1).toLowerCase()
 // }
-// console.log(generateBandName("blACK", "saLad"));
 
-function generateBandName(clothingColor, lastFoodEaten) {
-    let bandName = '';
+// console.log(generateBandName('piNK', 'PEACH'));
 
-    let color = capitalizeInitial(clothingColor);
-    let food = capitalizeInitial(lastFoodEaten);
+// 3
+// function generateBandName(clothingColor, lastFoodEaten) {
+//     let bandName = '';
 
-    return `The ${color} ${food}`;
+//     const color = capitalizeInitial(clothingColor);
+//     const food = capitalizeInitial(lastFoodEaten);
+
+//     return `The ${color} ${food}`;
+// }
+
+// function capitalizeInitial(capitalize) {
+//     return capitalize[0].toUpperCase() + capitalize.slice(1).toLowerCase()
+// }
+
+// console.log(generateBandName('bLuE', 'baGLE'));
+
+// 4
+function makeTasty(color) {
+    let fruitColor = '';
+
+    switch (color) {
+        case 'red' || 'Red':
+            return 'Chilli';
+            break;
+        case 'orange' || 'Orange':
+            return 'Tangerine';
+            break;
+        case 'yellow' || 'Yellow':
+            return 'Pineapple';
+        case 'green' || 'Green':
+            return 'Sage';
+            break;
+        case 'blue' || 'Blue':
+            return 'Blueberry';
+            break;
+        case 'purple' || 'Purple':
+            return 'Plum';
+    }
+
+    return fruitColor;
 }
 
-function capitalizeInitial(capitalize) {
-    return capitalize[0].toUpperCase() + capitalize.slice(1).toLowerCase()
-}
-
-console.log(generateBandName('bLuE', 'baGLE'));
+console.log(makeTasty('red'));
+console.log(makeTasty('purple'));
+console.log(makeTasty('blue'));
