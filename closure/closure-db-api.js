951822ce765a98a677c8954b3ef0f791e4cf36d4
Dana Shacham <dshacham22@gmail.com>
2019-09-12 11:36:24 +0200

    add fifth step

--- closure/closure-db-api.js
+++ closure/closure-db-api.js
@@ -1,3 +1,5 @@
+// Return a function reference
+
 // const init = () => {
 //     let db = [];
 
@@ -8,18 +10,29 @@
 //     return create;
 // }
 
+// const createInDb = init();
+
+// console.log(createInDb('testing'));
+// console.log(createInDb(4));
+// console.log(createInDb('how long is this'));
+
+// Expose more function references(provide an API)
+
+// Part one: refactor`init`
+
 const init = () => {
-    let db = [];
+    let api = [];
 
     const create = (element) => {
-        db.push(element);
-        return db.length;
+        api.push(element);
+        return api.length;
     }
+
     return create;
 }
 
-const createInDb = init();
+const createInApi = init();
 
-console.log(createInDb('testing'));
-console.log(createInDb(4));
-console.log(createInDb('how long is this'));
+console.log(createInApi('testing'));
+console.log(createInApi(4));
+console.log(createInApi('how long is this'));
